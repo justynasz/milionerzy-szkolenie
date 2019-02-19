586f7542126e0fb40f69ed82415183822d87b9cc
justynasz <justynaszczygiel@outlook.com>
2019-02-19 08:55:13 +0100

    Updates

---
+++ precache-manifest.e363754e3dd383b7340b47c8935869f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb9d86f0861c0e1f9461",
+    "url": "/milionerzy-szkolenie/static/js/runtime~main.cb9d86f0.js"
+  },
+  {
+    "revision": "f06c8c7ab273968cb3c2",
+    "url": "/milionerzy-szkolenie/static/js/main.f06c8c7a.chunk.js"
+  },
+  {
+    "revision": "d6f236311e060b461cf8",
+    "url": "/milionerzy-szkolenie/static/js/1.d6f23631.chunk.js"
+  },
+  {
+    "revision": "f06c8c7ab273968cb3c2",
+    "url": "/milionerzy-szkolenie/static/css/main.876e038a.chunk.css"
+  },
+  {
+    "revision": "fc5ad1c853b2942438032f3bf8d04127",
+    "url": "/milionerzy-szkolenie/index.html"
+  }
+];
